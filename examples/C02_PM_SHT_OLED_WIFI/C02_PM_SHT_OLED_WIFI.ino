8b6c6f295dcf6c21052612b76a6cc9b1ef7876d6
nitepone <admin@night.horse>
2022-03-05 15:00:05 -0500

    Local Graphing UI for OLED Example

--- examples/C02_PM_SHT_OLED_WIFI/C02_PM_SHT_OLED_WIFI.ino
+++ examples/C02_PM_SHT_OLED_WIFI/C02_PM_SHT_OLED_WIFI.ino
@@ -44,7 +44,7 @@ MIT License
 
 AirGradient ag = AirGradient();
 
-SSD1306Wire display(0x3c, SDA, SCL);
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
 
 // set sensors that you do not use to false
 boolean hasPM = true;
@@ -63,6 +63,23 @@ boolean connectWIFI = false;
 // change if you want to send the data to another server
 String APIROOT = "http://hw.airgradient.com/";
 
+// create structures for storing historical data to graph
+struct reading_history {
+  // history data
+  char* str_name;
+  int history_len;
+  int history_cur_pos;
+  int* history_table;
+  // sets scale for drawing graph
+  int y_range_max;
+  int y_range_min;
+  // sets color cutoffs
+  int y_range_warn;
+  int y_range_alarm;
+};
+struct reading_history pms_hist = {0};
+struct reading_history co2_hist = {0};
+
 void setup() {
   Serial.begin(9600);
 
@@ -70,54 +87,81 @@ void setup() {
   display.flipScreenVertically();
   showTextRectangle("Init", String(ESP.getChipId(), HEX), true);
 
-  if (hasPM) ag.PMS_Init();
-  if (hasCO2) ag.CO2_Init();
-  if (hasSHT) ag.TMP_RH_Init(0x44);
+  if (hasPM) {
+    asprintf(&(pms_hist.str_name), "PM2.5");
+    pms_hist.history_cur_pos = 0;
+    pms_hist.history_len = display.width();
+    pms_hist.history_table = (int*) malloc(sizeof(*(pms_hist.history_table)) *
+                                           pms_hist.history_len);
+    pms_hist.y_range_max = 50;
+    pms_hist.y_range_min = -2;
+    pms_hist.y_range_warn = 5;
+    pms_hist.y_range_alarm = 20;
+    ag.PMS_Init();
+  }
+  if (hasCO2) {
+    co2_hist.str_name = "CO2";
+    co2_hist.history_cur_pos = 0;
+    co2_hist.history_len = display.width();
+    co2_hist.history_table = (int*) malloc(sizeof(*(co2_hist.history_table)) *
+                                           co2_hist.history_len);
+    co2_hist.y_range_max = 2000;
+    co2_hist.y_range_min = 300;
+    co2_hist.y_range_warn = 800;
+    co2_hist.y_range_alarm = 1100;
+    ag.CO2_Init();
+  }
+  if (hasSHT) {
+    ag.TMP_RH_Init(0x44);
+  }
 
   if (connectWIFI) connectToWifi();
   delay(2000);
 }
 
 void loop() {
-
   // create payload
-
   String payload = "{\"wifi\":" + String(WiFi.RSSI()) + ",";
-
-  if (hasPM) {
-    int PM2 = ag.getPM2_Raw();
-    payload = payload + "\"pm02\":" + String(PM2);
-
-    if (inUSaqi) {
-      showTextRectangle("AQI", String(PM_TO_AQI_US(PM2)), false);
-    } else {
-      showTextRectangle("PM2", String(PM2), false);
-    }
-
-    delay(3000);
-
-  }
+  String overall_stats = "";
 
   if (hasCO2) {
-    if (hasPM) payload = payload + ",";
     int CO2 = ag.getCO2_Raw();
     payload = payload + "\"rco2\":" + String(CO2);
-    showTextRectangle("CO2", String(CO2), false);
-    delay(3000);
+    overall_stats += "CO2 " + String(CO2) + "\n";
+    if (co2_hist.history_table != NULL && co2_hist.history_len != 0) {
+      co2_hist.history_cur_pos = (co2_hist.history_cur_pos + 1) %
+                                  co2_hist.history_len;
+      co2_hist.history_table[co2_hist.history_cur_pos] = CO2;
+      showGraph(&co2_hist);
+    }
+
+    delay(5000);
+  }
+
+  if (hasPM) {
+    if (hasCO2) payload = payload + ",";
+    int PM2 = ag.getPM2_Raw();
+    payload = payload + "\"pm02\":" + String(PM2);
+    overall_stats += "PM " + String(PM2) + "\n";
+    if (inUSaqi) {
+      PM2 = PM_TO_AQI_US(PM2);
+    }
+    if (pms_hist.history_table != NULL && pms_hist.history_len != 0) {
+      pms_hist.history_cur_pos = (pms_hist.history_cur_pos + 1) %
+                                  pms_hist.history_len;
+      pms_hist.history_table[pms_hist.history_cur_pos] = PM2;
+      showGraph(&pms_hist);
+    }
+
+    delay(5000);
   }
 
   if (hasSHT) {
     if (hasCO2 || hasPM) payload = payload + ",";
     TMP_RH result = ag.periodicFetchData();
     payload = payload + "\"atmp\":" + String(result.t) + ",\"rhum\":" + String(result.rh);
-
-    if (inF) {
-      showTextRectangle(String((result.t * 9 / 5) + 32), String(result.rh) + "%", false);
-    } else {
-      showTextRectangle(String(result.t), String(result.rh) + "%", false);
-    }
-
-    delay(3000);
+    overall_stats += "Temp " + String(result.t) + "\n";
+    overall_stats += "Humid " + String(result.rh) + "\n";
   }
 
   payload = payload + "}";
@@ -136,8 +180,11 @@ void loop() {
     Serial.println(httpCode);
     Serial.println(response);
     http.end();
-    delay(21000);
   }
+
+  // show overall stats of last payload on screen
+  showTextSmall(overall_stats);
+  delay(20000);
 }
 
 // DISPLAY
@@ -149,8 +196,77 @@ void showTextRectangle(String ln1, String ln2, boolean small) {
   } else {
     display.setFont(ArialMT_Plain_24);
   }
-  display.drawString(32, 16, ln1);
-  display.drawString(32, 36, ln2);
+  display.drawString(0, 0, ln1);
+  display.drawString(0, 16, ln2);
+  display.display();
+}
+
+void showTextSmall(String str) {
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, str);
+  display.display();
+}
+
+void showGraph(struct reading_history* hist) {
+  int i;
+  int cur_pos;
+  int raw_y_value;
+  int disp_y_value;
+  int prev_disp_y_value;
+  int line_len;
+  int display_cur_pos;
+  float raw_y_to_disp_y_ratio;
+  char str_buf[32];
+
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawStringf(0, 0, str_buf,
+                      "%s\n%d",
+                      hist->str_name,
+                      hist->history_table[hist->history_cur_pos]);
+
+  display_cur_pos = (display.width() - 1);
+  raw_y_to_disp_y_ratio = (hist->y_range_max - hist->y_range_min) /
+                          display.height();
+  display.setColor(WHITE);
+  for (i = hist->history_len; i > 0; i--) {
+    // get raw y_value from table
+    cur_pos = (i + hist->history_cur_pos) % hist->history_len;
+    raw_y_value = hist->history_table[cur_pos];
+
+    // process y value into range
+    if (raw_y_value > hist->y_range_max) {
+      raw_y_value = hist->y_range_max;
+    }
+    if (raw_y_value < hist->y_range_min) {
+      raw_y_value = hist->y_range_min;
+    }
+
+    // process y value and line length
+    disp_y_value = (int)((raw_y_value - hist->y_range_min) /
+                         raw_y_to_disp_y_ratio);
+    disp_y_value = display.height() - disp_y_value;
+    if (i == hist->history_len) {
+      prev_disp_y_value = disp_y_value;
+    }
+    line_len = prev_disp_y_value - disp_y_value;
+    // draw line
+    display.drawLine(
+        display_cur_pos,
+        disp_y_value,
+        display_cur_pos,
+        disp_y_value + line_len);
+    display_cur_pos--;
+    if (display_cur_pos < 0) {
+      // prevent from going off display..
+      break;
+    }
+    prev_disp_y_value = disp_y_value;
+  }
+
   display.display();
 }
 
@@ -166,7 +282,6 @@ void connectToWifi() {
     ESP.restart();
     delay(5000);
   }
-
 }
 
 // Calculate PM2.5 US AQI
